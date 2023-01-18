f6a01e15e2958bfa8c7268c0dd79c7a9b5f0b64e
bladimirreveron <117594266+bladimirreveron@users.noreply.github.com>
2023-01-18 17:16:41 -0600

    included weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,20 @@ document.getElementById('log-in-button').onclick = function() {
         location.href='https://accounts.spotify.com/authorize?client_id=c801eb67890c4a1cac34bab0ba5b35a7&response_type=code&redirect_uri=https://andyanza.github.io/pitch/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
     }
 
+var weatherapi = "http://api.weatherapi.com/v1/current.json?key=0c2abbfc3ce4437ea44200408231801&q=Monterrey&aqi=yes"
+var name = document.querySelector(".name");
+var temp = document.querySelector(".temp");
+
+
+    fetch(weatherapi)
+ .then(response => response.json())   
+ .then(data => { 
+    var nameValue = data["location"]["name"];
+    var tempValue = data["current"]["temp_c"];
+
+    //name.innerHTML = "Location - "+nameValue;
+    temp.innerHTML = "Temp : "+tempValue;
+
+} )
+
+   // console.log(data))
