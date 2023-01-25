ff565a5281869c45f6cb4a2ba350d43647f4ce08
Andrea Ramirez <andrea.anza3@gmail.com>
2023-01-25 17:25:21 -0600

    Fixed signs names

--- assets/js/script.js
+++ assets/js/script.js
@@ -442,40 +442,40 @@ function zodiac_sign(day, month) {
   // valid range of a specified zodiac
   if (month == 12) {
     if (day < 22) astro_sign = "Sagittarius";
-    else astro_sign = "capricorn";
+    else astro_sign = "Capricorn";
   } else if (month == 01) {
-    if (day < 20) astro_sign = "capricorn";
-    else astro_sign = "aquarius";
+    if (day < 20) astro_sign = "Capricorn";
+    else astro_sign = "Aquarius";
   } else if (month == 02) {
     if (day < 19) astro_sign = "Aquarius";
-    else astro_sign = "pisces";
+    else astro_sign = "Pisces";
   } else if (month == 03) {
     if (day < 21) astro_sign = "Pisces";
-    else astro_sign = "aries";
+    else astro_sign = "Aries";
   } else if (month == 04) {
     if (day < 20) astro_sign = "Aries";
-    else astro_sign = "taurus";
+    else astro_sign = "Taurus";
   } else if (month == 05) {
     if (day < 21) astro_sign = "Taurus";
-    else astro_sign = "gemini";
+    else astro_sign = "Gemini";
   } else if (month == 06) {
     if (day < 21) astro_sign = "Gemini";
-    else astro_sign = "cancer";
+    else astro_sign = "Cancer";
   } else if (month == 07) {
     if (day < 23) astro_sign = "Cancer";
-    else astro_sign = "leo";
+    else astro_sign = "Leo";
   } else if (month == 08) {
     if (day < 23) astro_sign = "Leo";
-    else astro_sign = "virgo";
+    else astro_sign = "Virgo";
   } else if (month == 09) {
     if (day < 23) astro_sign = "Virgo";
-    else astro_sign = "libra";
+    else astro_sign = "Libra";
   } else if (month == 10) {
     if (day < 23) astro_sign = "Libra";
-    else astro_sign = "scorpio";
+    else astro_sign = "Scorpio";
   } else if (month == 11) {
-    if (day < 22) astro_sign = "scorpio";
-    else astro_sign = "sagittarius";
+    if (day < 22) astro_sign = "Scorpio";
+    else astro_sign = "Sagittarius";
   }
 }
 
